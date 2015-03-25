00d5c45dd3ad5120c56a1409450094c6bc189c9b
Iulia Banghea <iulia609@gmail.com>
2015-03-25 01:57:59 +0000

    Home changes and intruder world

--- partA2.m
+++ partA2.m
@@ -5,7 +5,7 @@ function [] = partA()
     TIME_STEP = 64;
     N = 8;
     GOAL_DISTANCE = 300;
-    DEFAULT_SPEED = 3;
+    DEFAULT_SPEED = 4;
     K = 0.01;
     x = 0;
     y = 0;
@@ -19,36 +19,38 @@ function [] = partA()
     r1 = 0;
     while wb_robot_step(TIME_STEP) ~= -1
         
-        for i=1:100  % OR something that ends the so-called 'job'
+        % read all distance sensors
+            sensor_values = get_sensor_values();
         
-            % read all distance sensors
-                sensor_values = get_sensor_values();
-            
-            % sum the values of left sensors
-                left_sensors = sum(sensor_values(1:3));
+        % sum the values of left sensors
+            left_sensors = sum(sensor_values(1:3));
 
-            % proportional error control
-                error = K*(left_sensors - GOAL_DISTANCE*2);
+        % proportional error control
+            error = K*(left_sensors - GOAL_DISTANCE*2);
 
-            % % set speeds
-            l1temp = wb_differential_wheels_get_left_encoder();
-            r1temp = wb_differential_wheels_get_right_encoder();
-            d1 =  (l1temp - l1) ;
-            d2 =   (r1temp - r1) ;
-            l1 =  l1temp; %*0.0008 + error
-            r1 =  r1temp; %*0.0008 - error
-            left_speed = DEFAULT_SPEED + error; 
-            right_speed = DEFAULT_SPEED - error;
-            % disp('PRe phi');
-            % phi
-            % disp('######');
-            wb_differential_wheels_set_speed(left_speed,right_speed);
-            [x, y, phi] = odometry( x, y, phi, d1, d2,TIME_STEP)
-            % muahahahahahahahahahah
-            wb_robot_step(TIME_STEP);  
+        % % set speeds
+        l1temp = wb_differential_wheels_get_left_encoder();
+        r1temp = wb_differential_wheels_get_right_encoder();
+        d1 =  (l1temp - l1) ;
+        d2 =   (r1temp - r1) ;
+        l1 =  l1temp; %*0.0008 + error
+        r1 =  r1temp; %*0.0008 - error
+        left_speed = DEFAULT_SPEED + error; 
+        right_speed = DEFAULT_SPEED - error;
+        % disp('PRe phi');
+        % phi
+        % disp('######');
+        wb_differential_wheels_set_speed(left_speed,right_speed);
+        [x, y, phi] = odometry( x, y, phi, d1, d2,TIME_STEP);
+        [x*100, y*100, phi]
+        % muahahahahahahahahahah
+        wb_robot_step(TIME_STEP);  
+        
+        
+        % is it home yet?   TODO: make it actually go there
+        if ((x*100 < 0.9 && y*100 < 0.9 && x*100 >= 0 && y*100 >= 0) || (x*100 > -0.9 && y*100 > -0.9 && x*100 <= 0 && y*100 <= 0))
+            disp "homeeee"
         end
-            
-        % and maybe add the 'homing' function here? whatever that's supposed to do
             
     end
 
